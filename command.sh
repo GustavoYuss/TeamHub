9ed269abbda83e594c177f7d02ff0638290be2be
Gustavo <yusgus02@gmail.com>
2024-06-23 20:13:51 -0600

    Agregar comandos de construccion para bash

--- command.sh
+++ command.sh
@@ -1,28 +1,79 @@
 #!/bin/bash
 
-cd TeamHubSericeFileRest
-docker build  -t grpcfileservice .
+# Cambiar al directorio TeamHubServiceFiles y construir la imagen Docker
+cd TeamHubServicesFiles || exit 1
+docker build -t grpcfileservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamHubServiceProjects
-docker build  -t projectservice .
+# Cambiar al directorio TeamHubLogService y construir la imagen Docker
+cd ../TeamHubLogService || exit 1
+docker build -t logservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamHubServicesFiles
-docker build  -t fileservice .
+# Cambiar al directorio TeamHubServiceProjects y construir la imagen Docker
+cd ../TeamHubServiceProjects || exit 1
+docker build -t projectservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamHubServiceUser
-docker build  -t userservice .
+# Cambiar al directorio TeamHubServiceFileRest y construir la imagen Docker
+cd ../TeamHubServiceFileRest || exit 1
+docker build -t fileservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamHubTaskService
-docker build  -t taskservice .
+# Cambiar al directorio TeamHubServiceUser y construir la imagen Docker
+cd ../TeamHubServiceUser || exit 1
+docker build -t userservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamsHubWebClient
-docker build  -t webclient .
+# Cambiar al directorio TeamHubTaskService y construir la imagen Docker
+cd ../TeamHubTasksService || exit 1
+docker build -t taskservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamsHubAPIGateway
-docker build  -t apigateway .
+# Cambiar al directorio TeamsHubWebClient y construir la imagen Docker
+cd ../TeamsHubWebClient || exit 1
+docker build -t webclient .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
-cd ../TeamHubSessionsServices
-docker build  -t sessionservice .
+# Cambiar al directorio TeamsHubAPIGateway y construir la imagen Docker
+cd ../TeamsHubAPIGateway || exit 1
+docker build -t apigateway .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
+# Cambiar al directorio TeamHubSessionsServices y construir la imagen Docker
+cd ../TeamHubSessionsServices || exit 1
+docker build -t sessionservice .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
+
+# Cambiar al directorio TeamHubDataBase y construir la imagen Docker
+cd ../TeamHubDataBase || exit 1
+docker build -t teamhub_db .
+if [ $? -ne 0 ]; then
+  exit $?
+fi
+
+# Ejecutar docker-compose build y docker-compose up
 docker-compose build
+if [ $? -ne 0 ]; then
+  exit $?
+fi
+
 docker-compose up
